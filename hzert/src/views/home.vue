a4088f7dbc21128d697f000044207bf91b3394bf
hzertz0 <139808204+hzertz0@users.noreply.github.com>
2026-03-16 14:02:12 -0400

    ye

--- hzert/src/views/home.vue
+++ hzert/src/views/home.vue
@@ -1,11 +1,25 @@
 <template>
     <div>
-        <p>hi</p>
+        <p v-for="business in applications">The {{ business.agency}} is looking for {{ business.civil_service_title }}</p>
     </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+const applications = ref([])
+async function getData() {
+  try {
+    const response = await fetch('https://data.cityofnewyork.us/resource/kpav-sd4t.json')
+    const data = await response.json()
+    applications.value = data
+  } catch (error) {
+    console.log(error)
+  }
+}
 
+onMounted(() => {
+  getData()
+})
 </script>
 
 <style scoped>
