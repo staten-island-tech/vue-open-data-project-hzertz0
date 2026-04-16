a7f93bcae8ae1d5a81c794fccdaccc13a1dc3a5a
hzertz0 <139808204+hzertz0@users.noreply.github.com>
2026-04-16 08:15:48 -0400

    job

--- hzert/src/router/index.js
+++ hzert/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import home from '@/views/home.vue'
 import commonjobs from '@/views/commonjobs.vue'
+import jobdemand from '@/views/jobdemand.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/commonjobs',
       name: 'common',
       component:commonjobs
+    },
+        {
+      path: '/jobdemand',
+      name: 'demand',
+      component:jobdemand
     },
   ],
 })
